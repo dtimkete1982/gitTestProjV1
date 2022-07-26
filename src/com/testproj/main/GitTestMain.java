e8c210bd9e3db385b87aaa393d90c8d7659f0146
semeg <semeg@192.168.0.5>
2022-07-26 07:25:59 -0700

    initial commit

---
+++ src/com/testproj/main/GitTestMain.java
@@ -0,0 +1,12 @@
+package com.testproj.main;
+
+public class GitTestMain {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Test Main Method");
+
+	}
+
+}
